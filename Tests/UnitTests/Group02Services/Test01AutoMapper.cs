aa50c75249482604673f03f108414e60fd0f55a8
JonSmith <jon.smith@selectiveanalytics.com>
2014-05-20 16:29:13 +0100

    Minor test to check how automapper works

---
+++ Tests/UnitTests/Group02Services/Test01AutoMapper.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using AutoMapper;
+using NUnit.Framework;
+using Tests.Helpers;
+
+namespace Tests.UnitTests.Group02Services
+{
+    class Test01AutoMapper
+    {
+        private class Data
+        {
+            public int [] Ints { get; set; }
+
+            public int GetSum { get { return Ints.Sum(); } }
+        }
+
+        private class Dto
+        {
+            public int IntsCount { get; set; }
+            public int Sum { get; set; }
+        }
+
+        [Test]
+        public void Check01DirectReferenceOk()
+        {
+
+            //SETUP  
+            var data = new Data {Ints = new[] {1, 2, 3}};
+
+            //ATTEMPT
+            Mapper.CreateMap<Data, Dto>();
+            var dto = Mapper.Map<Data, Dto>(data);
+
+            //VERIFY
+            dto.IntsCount.ShouldEqual(3);
+            dto.Sum.ShouldEqual(6);
+        }
+    }
+}
