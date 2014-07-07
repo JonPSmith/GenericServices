aede14b5214aaa2fee17d15e309dbc31cbc697b2
JonSmith <jon.smith@selectiveanalytics.com>
2014-07-07 10:41:46 +0100

    Added TraceGenericLogger for use in Azure

---
+++ Tests/Helpers/TraceGenericLogger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using GenericServices.Logger;
+
+namespace Tests.Helpers
+{
+
+    public class TraceGenericLogger : IGenericLogger
+    {
+
+        private readonly string _loggerName;
+
+        /// <summary>
+        /// Controls whether Verbose is written to Trace.
+        /// Defaults to excluding Verbose
+        /// </summary>
+        public bool IncludeVerbose { get; set; }
+
+        public TraceGenericLogger(string name)
+        {
+            _loggerName = name;
+        }
+
+        public void Verbose(object message)
+        {
+            if (IncludeVerbose)
+                Trace.TraceInformation("{0}: {1}", _loggerName, message);
+        }
+
+        public void VerboseFormat(string format, params object[] args)
+        {
+            Verbose(string.Format(format, args));
+        }
+
+        public void Info(object message)
+        {
+            Trace.TraceInformation("{0}: {1}", _loggerName, message);
+        }
+
+        public void InfoFormat(string format, params object[] args)
+        {
+            Info(string.Format(format, args));
+        }
+
+        public void Warn(object message)
+        {
+            Trace.TraceWarning("{0}: {1}", _loggerName, message);
+        }
+
+        public void WarnFormat(string format, params object[] args)
+        {
+            Warn(string.Format(format, args));
+        }
+
+        public void Error(object message)
+        {
+            Trace.TraceError("{0}: {1}", _loggerName, message);
+        }
+
+        public void ErrorFormat(string format, params object[] args)
+        {
+            Error(string.Format(format, args));
+        }
+
+        public void Critical(object message)
+        {
+            Trace.TraceError("{0}: {1}", _loggerName, message);
+        }
+
+        public void Critical(object message, Exception ex)
+        {
+            Trace.TraceError("{0}, expection {1}: {2}\n{3}", _loggerName, ex.GetType().Name, message, ex.StackTrace);
+        }
+
+        public void CriticalFormat(string format, params object[] args)
+        {
+            Critical(string.Format(format, args));
+        }
+
+    }
+}
---
+++ Tests/UnitTests/Group05GenericLogger/Test06TraceGenericLogger.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using GenericServices;
+using GenericServices.Logger;
+using NUnit.Framework;
+using Tests.Actions;
+using Tests.Helpers;
+
+namespace Tests.UnitTests.Group05GenericLogger
+{
+    class Test06TraceGenericLogger
+    {
+        private readonly StringBuilder _loggedData = new StringBuilder();
+
+        private IGenericLogger _loggerA;
+        private IGenericLogger _loggerB;
+
+        [TestFixtureSetUp]
+        public void SetupFixture()
+        {
+            var memoryWriter = new StringWriter(_loggedData);
+            Trace.Listeners.Add(new TextWriterTraceListener(memoryWriter));
+
+            GenericLoggerFactory.SetLoggerMethod = name => new TraceGenericLogger(name);
+            _loggerA = GenericLoggerFactory.GetLogger("A");
+            _loggerB = GenericLoggerFactory.GetLogger("B");
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _loggedData.Clear();
+        }
+
+        [Test]
+        public void Check01LoggerAOk()
+        {
+
+            //SETUP  
+
+            //ATTEMPT
+            _loggerA.Info("a message");
+
+            //VERIFY
+            Trace.Flush();
+            var logs = _loggedData.ToString().Split('\n').Where( x => x != string.Empty).Select(x => x.Trim()).ToArray();
+            logs.Length.ShouldEqual(1);
+            logs[0].ShouldEndWith("A: a message");
+        }
+
+        [Test]
+        public void Check02LoggerBOk()
+        {
+
+            //SETUP  
+
+            //ATTEMPT
+            _loggerB.Info("a message");
+
+            //VERIFY
+            Trace.Flush();
+            var logs = _loggedData.ToString().Split('\n').Where(x => x != string.Empty).Select(x => x.Trim()).ToArray();
+            logs.Length.ShouldEqual(1);
+            logs[0].ShouldEndWith("B: a message");
+        }
+
+        [Test]
+        public void Check03LoggerAandBOk()
+        {
+
+            //SETUP  
+
+            //ATTEMPT
+            _loggerA.Info("message 1");
+            _loggerB.Info("message 2");
+
+            //VERIFY
+            Trace.Flush();
+            var logs = _loggedData.ToString().Split('\n').Where(x => x != string.Empty).Select(x => x.Trim()).ToArray();
+            logs.Length.ShouldEqual(2);
+            logs[0].ShouldEndWith("A: message 1");
+            logs[1].ShouldEndWith("B: message 2");
+        }
+
+        //----------------------------------------------------------
+
+        [Test]
+        public void Check05LoggerVerboseNotIncludedOk()
+        {
+
+            //SETUP  
+
+            //ATTEMPT
+            _loggerA.Verbose("a message");
+
+            //VERIFY
+            Trace.Flush();
+            var logs = _loggedData.ToString().Split('\n').Where(x => x != string.Empty).Select(x => x.Trim()).ToArray();
+            logs.Length.ShouldEqual(0);
+        }
+
+        [Test]
+        public void Check06LoggerVerboseIncludedOk()
+        {
+
+            //SETUP  
+            ((TraceGenericLogger)_loggerA).IncludeVerbose = true;
+
+            //ATTEMPT
+            _loggerA.Verbose("a message");
+
+            //VERIFY
+            Trace.Flush();
+            var logs = _loggedData.ToString().Split('\n').Where(x => x != string.Empty).Select(x => x.Trim()).ToArray();
+            logs.Length.ShouldEqual(1);
+            logs[0].ShouldEndWith("A: a message");
+        }
+
+
+        //-----------------------------------------------------------
+
+        [Test]
+        public void Check10LogExceptionOk()
+        {
+
+            //SETUP  
+
+            //ATTEMPT
+            try
+            {
+                throw new InvalidOperationException("This is the exception message.");
+            }
+            catch (Exception ex)
+            {
+                _loggerA.Critical("an exception", ex);
+            }
+
+
+            //VERIFY
+            Trace.Flush();
+            var logs = _loggedData.ToString().Split('\n').Where(x => x != string.Empty).Select(x => x.Trim()).ToArray();
+            logs.Length.ShouldEqual(2);
+            logs[0].ShouldEndWith("A, expection InvalidOperationException: an exception");
+        }
+
+        [Test]
+        public void Check20ActionDataBaseLoggerOk()
+        {
+
+            //SETUP  
+            var data = new GTestActionData
+            {
+                SecondsBetweenIterations = 0,
+                NumIterations = 1
+            };
+            var action = new GTestAction();
+
+            //ATTEMPT
+            action.DoAction(new MockActionComms(), data);
+
+            //VERIFY
+            Trace.Flush();
+            var logs = _loggedData.ToString().Split('\n').Where(x => x != string.Empty).Select(x => x.Trim()).ToArray();
+            logs.Length.ShouldEqual(2);
+            logs[0].ShouldEndWith("GTestAction: Action has started. Will run for 0.0 seconds.");
+
+        }
+
+    }
+}
