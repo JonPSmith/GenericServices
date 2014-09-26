49f406cc6475a097e2041b145620e9086c8979be
JonSmith <jon.smith@selectiveanalytics.com>
2014-09-26 08:51:28 +0100

    Made version of SaveChangesWithChecking that takes DbContext

--- GenericServices/NuGet/AssemblyVersionPart.cs
+++ GenericServices/NuGet/AssemblyVersionPart.cs
@@ -42,4 +42,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta14")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta3-002")]
