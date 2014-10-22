d26f53cbd5867aefd745ac87709315a4d38f9c5c
JonSmith <jon.smith@selectiveanalytics.com>
2014-10-22 15:26:31 +0100

    Revert "Removed unused Unit Test and fixed csproj"
    This reverts commit 6c845ff1c3fd8ca6aa94ac339ccc37002859cefc.

---
+++ Tests/UnitTests/Group15CrudServiceFinder/Test01DecodeToTypes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericServices.Core.Internal;
+using GenericServices.Services.Concrete;
+using NUnit.Framework;
+using Tests.DataClasses.Concrete;
+using Tests.Helpers;
+
+namespace Tests.UnitTests.Group15CrudServiceFinder
+{
+    class Test01DecodeToTypes
+    {
+
+        [Test]
+        public void Test01DecodeListOk()
+        {
+
+            //SETUP
+            var timer = new Stopwatch();
+
+            //ATTEMPT
+            timer.Start();
+            var service = DecodeToService<ListService>.CreateCorrectService<Post>(WhatItShouldBe.SyncAnything, new object[] { null });
+            timer.Stop();
+
+            //VERIFY
+            ExtendAsserts.ShouldNotEqualNull(service);
+            ExtendAsserts.IsA<ListService<Post>>(service);
+            Console.WriteLine("took {0:f3} ms", 1000.0 * timer.ElapsedTicks / Stopwatch.Frequency);
+        }
+
+    }
+}
