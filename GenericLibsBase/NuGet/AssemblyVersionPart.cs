be59760027ccf64aa4f3df0b0cc1a3450d0c27c7
JonSmith <jon.smith@selectiveanalytics.com>
2014-10-28 10:01:09 +0000

    Sorted porblems in NuGet specs

--- GenericLibsBase/NuGet/AssemblyVersionPart.cs
+++ GenericLibsBase/NuGet/AssemblyVersionPart.cs
@@ -43,4 +43,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta5-001")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta5-002")]
