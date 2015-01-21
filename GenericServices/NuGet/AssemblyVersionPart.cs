726e0ee107ca821313dadf6f85d2e9283de2c53b
JonSmith <jon.smith@selectiveanalytics.com>
2015-01-21 10:00:08 +0000

    Updated to latest DelegateDecompiler.EF and issued as NuGet package

--- GenericServices/NuGet/AssemblyVersionPart.cs
+++ GenericServices/NuGet/AssemblyVersionPart.cs
@@ -40,6 +40,6 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
